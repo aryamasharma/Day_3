1deaa89e0281455e93c358833a90ac7effef6f96
aryamasharma <aryama.subbu@gmail.com>
2022-07-05 17:46:00 +0545

    :tada: day 3

---
+++ basic/string.py
@@ -0,0 +1,17 @@
+print(9-3)
+print(9/3)
+print (9//3)
+print (9*3)
+print(2**3)
+print(5%2)
+print('U0001F601')
+print("\U0001F601")
+print(round(2*3.14,3))
+print("cotton"+"candy")
+print ("lol"+str(20))
+
+# variable in python:
+a=11
+print ("the first value of a is: --->",a)
+a= "Apple"
+print("the second value of a is: =", a)
