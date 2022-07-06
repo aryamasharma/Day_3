89b1053ea1241b239da6b8209fa7b2907a0f1d99
aryamasharma <aryama.subbu@gmail.com>
2022-07-06 17:04:21 +0545

    :hammer: two inputs

---
+++ basic/string02.py
@@ -0,0 +1,7 @@
+first_name , second_name = input("please provide your full name(space seperated): ").split(" ")
+print("your full name is ---> " +first_name + " "+ second_name)
+first_name , second_name = input("please provide your full name(comma seperated): ").split(",")
+print("your full name is ---> " +first_name + " "+ second_name)
+name_type = type(first_name)
+print("type of firstname is ", name_type)
+print("type of second is ", type(second_name))
